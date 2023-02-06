10daeccad09d702232743cbacf70f5bdf4c03c50
Praveen Kumar <praveen.r@indiumsoft.com>
2023-02-06 09:33:48 +0530

    Added Custom Font, Working On Styles 06-02-2023

--- src/Components/CategoryCard/index.js
+++ src/Components/CategoryCard/index.js
@@ -1,10 +1,11 @@
 const CategoryCard = (props) => {
-    // const { } = props;
+    const { name, url, className, bodyClassName, imgClassName, nameClassName, subNameClassName } = props;
 
-    return <div>
-        <div>
-            <h6>HATS</h6>
-            <p>SHOP NOW</p>
+    return <div className={`basic-category-card ${className}`}>
+        <img className={`basic-category-card-img ${imgClassName}`} src={url}></img>
+        <div className={`basic-category-card-body ${bodyClassName}`}>
+            <h6 className={`basic-category-card-name ${nameClassName}`}>{name}</h6>
+            <p className={`basic-category-card-subname ${subNameClassName}`}>SHOP NOW</p>
         </div>
     </div>
     
--- src/Container/HomePage/index.js
+++ src/Container/HomePage/index.js
@@ -1,12 +1,45 @@
-import CategoryCard from "../../Components/CategoryCard"
+import CategoryCard from "../../Components/CategoryCard";
+import image from '../../Assets/Images/Image1.jpg';
 
 const HomePage = (props) => {
+
+    const categoryItems = [
+        {
+            id: 1,
+            name: 'HATS',
+            subName: 'SHOP NOW',
+            url: image
+        }, 
+        {
+            id: 2,
+            name: 'JACKETS',
+            subName: 'SHOP NOW',
+            url: ''
+        },
+        {
+            id: 3,
+            name: 'SNEAKERS',
+            subName: 'SHOP NOW',
+            url: ''
+        },
+        {
+            id: 4,
+            name: `MEN'S`,
+            subName: 'SHOP NOW',
+            url: ''
+        },
+        {
+            id: 5,
+            name: `WOMEN'S`,
+            subName: 'SHOP NOW',
+            url: ''
+        }
+    ];
+
+    const categoryElements = categoryItems?.map((eE) => <CategoryCard key={eE?.id} className={eE?.name === `MEN'S` && 'mens'} name={eE?.name} url={eE?.url} />);
+
     return <div className="basic-home-page">
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
+        {categoryElements}
     </div>;
 }
 
